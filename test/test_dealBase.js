5934b31cfb19eed0314ea0e2932618114092b93c
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2020-03-11 12:58:51 +0300

    full tests for propose reviewer state and its transitions

--- test/test_dealBase.js
+++ test/test_dealBase.js
@@ -4,8 +4,11 @@ const DealToken = artifacts.require("DealToken");
 contract('Deal. Base Test', async accounts => {
     const States = {
         INIT: 0,
-        PROPOSED_REVIWER: 1
+        PROPOSED_REVIWER: 1,
+        RFP: 2,
+        DEPOSIT_WAIT: 3,
     }
+    const zeroAddress = 0;
 
     const client = accounts[0];
     const contractor = accounts[1];
@@ -59,33 +62,76 @@ contract('Deal. Base Test', async accounts => {
     it("should fail transition to PROPOSED_REVIWER", async() => {
         //wrong access
         await expectThrow(
-            dealContract.proposeReviewer(contractor, 5, 1000, {from: contractor})
+            dealContract.proposeReviewer(reviewer1, 5, 1000, {from: contractor})
         )
         // wrong fee
         await expectThrow(
-            dealContract.proposeReviewer(contractor, 12, 1000, {from: client})
+            dealContract.proposeReviewer(reviewer1, 12, 1000, {from: client})
         )
         // wrong decision duration
         await expectThrow(
-            dealContract.proposeReviewer(contractor, 5, 0, {from: client})
+            dealContract.proposeReviewer(reviewer1, 5, 0, {from: client})
         )
     })
 
-    it("should change state to PROPOSED_REVIEWER", async() => {
-        contractorFee = 5 // 0.05%
+    it("should change state to PROPOSED_REVIEWER proposing reviewer1", async() => {
+        reviewerFee = 5 // 0.05%
         reviwerDecisionDuration = 60 * 60 * 24 * 3 // 3 days
-        await dealContract.proposeReviewer(contractor, contractorFee, reviwerDecisionDuration, {from: client});
+        await dealContract.proposeReviewer(reviewer1, reviewerFee, reviwerDecisionDuration, {from: client});
 
         contractState = await dealContract.currentState.call();
         assert.equal(contractState, States.PROPOSED_REVIWER)
     })
 
     it("should reset reviewer params", async() => {
-        contractorFee = 5 // 0.05%
+        reviewerFee = 5 // 0.05%
         reviwerDecisionDuration = 60 * 60 * 24 * 5 // 5 days
-        await dealContract.proposeReviewer(contractor, contractorFee, reviwerDecisionDuration, {from: client});
+        await dealContract.proposeReviewer(reviewer1, reviewerFee, reviwerDecisionDuration, {from: client});
 
         contractState = await dealContract.currentState.call();
         assert.equal(contractState, States.PROPOSED_REVIWER)        
+    })
+
+    it("should fail decline reviewer coniditions", async() => {
+        // wrong access
+        await expectThrow(
+            dealContract.declineReviewConditions({from: contractor})
+        )
+    })
+
+    it("should decline reviewer conditions", async() => {
+        tx = await dealContract.declineReviewConditions({from: reviewer1})
+
+        contractState = await dealContract.currentState.call();
+        assert.equal(contractState, States.INIT)
+
+        statedReviewer = tx.logs[0].args.reviewer;
+        assert.equal(statedReviewer, zeroAddress)
+    })
+
+    it("should change state to PROPOSED_REVIEWER proposing reviewer2", async() => {
+        reviewerFee = 5 // 0.05%
+        reviwerDecisionDuration = 60 * 60 * 24 * 3 // 5 days
+        await dealContract.proposeReviewer(reviewer2, reviewerFee, reviwerDecisionDuration, {from: client});
+
+        contractState = await dealContract.currentState.call();
+        assert.equal(contractState, States.PROPOSED_REVIWER)
+    })
+
+    it("should accept reviewer conditions by reviewer2 and move state to RFP", async() => {
+        tx = await dealContract.acceptReviewConditions({from: reviewer2})
+
+        contractState = await dealContract.currentState.call();
+        assert.equal(contractState, States.RFP)
+
+        statedReviewer = tx.logs[0].args.reviewer;
+        assert.equal(statedReviewer, reviewer2)
+    })
+
+    it("should fail review decline by reviewer2", async() => {
+        // wrong state
+        await expectThrow(
+            dealContract.declineReviewConditions({from: reviewer2})
+        )
     })
 })
